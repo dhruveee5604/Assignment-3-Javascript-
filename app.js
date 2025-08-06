3e27995d1ddc659db403d98b1aa464b01d6c83bf
Dhruvita Bhatt <200612383@student.georgianc.on.ca>
2025-08-06 10:54:44 -0400

    Initial commit - Cat Gallery with API

---
+++ app.js
@@ -0,0 +1,26 @@
+const button = document.getElementById('loadCats');
+const gallery = document.getElementById('gallery');
+
+button.addEventListener('click', () => {
+  gallery.innerHTML = ''; // clear old images
+  loadCats(5); // load 5 cat images
+});
+
+function loadCats(count) {
+  for (let i = 0; i < count; i++) {
+    fetch("https://api.thecatapi.com/v1/images/search", {
+      headers: {
+        "x-api-key": "live_imx3jMJalWXYMSNOCxLRCBE38oLProoe8VEDbrlb1dVJwTWCNNBy8HyoWRq0cRR3"
+      }
+    })
+    .then(res => res.json())
+    .then(data => {
+      const img = document.createElement('img');
+      img.src = data[0].url;
+      gallery.appendChild(img);
+    })
+    .catch(err => {
+      console.error('Error loading cat:', err);
+    });
+  }
+}
